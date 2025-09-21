855f8217a03ea65a0d723474d66e49d5e87fcdb3
felip2018 <felipegarxon@hotmail.com>
2025-09-20 20:52:51 -0500

    build initial service

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -1,11 +1,13 @@
 import {Request, Response} from 'express';
+import notificationService from "../services/notification.service";
 
 class NotificationController {
     public async send(req: Request, res: Response) {
         try {
+            const result = await notificationService.sendEmail();
             return res.status(200).json({
                 message: 'ok',
-                body: {}
+                body: result
             });
         } catch (e) {
             return res.status(409).json(e);
--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,13 +1,9 @@
-import {IEvent} from "../../../p01_database_lib";
-import {Event} from "../../../p01_database_lib";
-import {Op} from "../../../p01_database_lib";
 
 class NotificationService {
-    public save(event: IEvent) {
+    public sendEmail() {
         return new Promise((resolve, reject) => {
             try {
-                // @ts-ignore
-                const result = Event.create(event);
+                const result = "email sended succcesfully";
                 resolve(result);
             } catch (e) {
                 reject(e);
@@ -15,28 +11,6 @@ class NotificationService {
         })
     }
 
-    public report(startDate: string, endDate: string) {
-        return new Promise(async(resolve, reject) => {
-            try {
-                const from = `${startDate} 00:00:00`;
-                const to = `${endDate} 11:59:59`;
-                const result = await Event.findAll({
-                    where: {
-                        created_at: {
-                            [Op.gte]: from,
-                            [Op.lte]: to,
-                        },
-                    },
-                    include: [
-                        {association: 'User'}
-                    ]
-                });
-                resolve(result);
-            } catch (e) {
-                reject(e);
-            }
-        })
-    }
 }
 
 const eventService = new NotificationService();
